f418d432852ace44db5dd99303bad0f7112f2d7e
Loréleine Le Clerc <loreleine.leclerc@decathlon.com>
2017-12-05 11:35:39 +0100

    begin node and internet fundamentals

---
+++ 03_backend_developement/02_node_&_internet_fundamentals/01_request_the_world/talk_with_postman_echo.js
@@ -0,0 +1,27 @@
+// A First Query
+
+function callback(result){
+
+}
+
+curl -X GET https://postman-echo.com/get "01_postman_api_call.result"
+
+function simpleGet(callback){
+
+  return ;
+}
+
+function simpleGetWithParams(callback){
+  return ;
+}
+
+function validateTimestamp(callback){
+  return ;
+}
+
+
+module.exports = {
+  simpleGet: simpleGet(),
+  simpleGetWithParams: simpleGetWithParams(),
+  validateTimestamp: validateTimestamp()
+}
